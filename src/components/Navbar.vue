8a3059d004b56d97dbd4de4eaa5bddfe80419ec6
Amanda Abreu <amandaabreu045@gmail.com>
2026-03-11 16:56:29 -0300

    Implementando Vue Router

---
+++ src/components/Navbar.vue
@@ -0,0 +1,16 @@
+<template>
+    <nav>
+    <router-link to="/">
+        <img src="" id="logo">
+    </router-link> 
+    <router-link to="/">Home</router-link> |
+    <router-link to="/pedidos">Pedidos</router-link>
+  </nav>
+</template>
+
+<script setup>
+    name: "Navbar"
+</script>
+
+
+<style></style>
